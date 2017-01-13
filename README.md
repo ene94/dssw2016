357a395cdcca1a861e3660afc3b2acee19bed693
Eneritz <ene_txiki94@hotmail.com>
2017-01-13 19:57:59 +0100

    Tarea 6 terminada

--- README.md
+++ README.md
@@ -1,32 +1,32 @@
 # DSSW 2016 Eneritz #
 Asignatura Desarrollo de Software Seguro en la Web
 
-Proyecto accesible a trav&eaccute;s de GAE: http://dssw2016eneritz.appspot.com/
+Proyecto accesible a trav&eacute;s de GAE: http://dssw2016eneritz.appspot.com/
 
 ## Tarea 1: Comenzando con Python y GAE ##
 
 * Descargar, instalar y configurar Google App Engine, Git, Python en el ordenador personal.
-* Crear la primera aplicacion Hola Mundo!
+* Crear la primera aplicaci&oacute;n Hola Mundo!
 * Obtener la pagina html desde un archivo y cargarla como template.
-* Ampliarla a tres idiomas: euskara, ingl&eaccute;s y español.
-* Añadir una imagen.
-* Añadir un CSS.
+* Ampliarla a tres idiomas: euskara, ingl&eacute;s y espa&ntilde;ol.
+* A&ntilde;adir una imagen.
+* A&ntilde;adir un CSS.
 
 ## Tarea 2: Lado cliente ##
 
 * Crear formulario.
-* Script de validacion (en el lado cliente) del formulario con HTML y jQuery.  
+* Script de validaci&oacute;n (en el lado cliente) del formulario con HTML y jQuery.  
 
 **8h aproximadamente realizar las tareas 1 y 2**
 
 ## Tarea 3: Lado servidor ##
 
-* Agregar validacion en el lado del servidor con RE
-* Optimizar el codigo para version multilingue
-* Paso de parametros para inicializar campos del HTML
+* Agregar validaci&oacute;n en el lado del servidor con RE
+* Optimizar el c&oacute;digo para version multilingue
+* Paso de par&aacute;metros para inicializar campos del HTML
 * Agregar usuarios a la base de datos
 * Verificar si el usuario ya existe antes de agregarlo
-* Ampliar a version multilingue de las nuevas paginas
+* Ampliar a versi&oacute;n multilingue de las nuevas p&aacute;ginas
 * --> FALTA: Agregar una foto en el registro
 * --> FALTA: Mostrar la foto en la tabla de usuarios
 
@@ -34,33 +34,39 @@ Proyecto accesible a trav&eaccute;s de GAE: http://dssw2016eneritz.appspot.com/
 
 ## Tarea 4: AJAX con JavaScript ##
 
-* Validacion asincrona del email funcionando en los 3 idiomas
+* Validaci&oacute;n as&iacute;ncrona del email funcionando en los 3 idiomas
 
 **1h aproximadamente realizar la tarea 4**
 
 ## Tarea 5: Web Services ##
 
-* Nueva pagina html con formulario para introducir direccion
-* Peticion al servicio Google Maps a trav�s de su API
-* Visualizacion del mapa con marcador el la ciudad indicada.
-* Nueva pagina para iniciar sesion con cuenta de Google
-* Inicio de sesion con Google mostrando informacion e imagen
-* Cierre de sesion de Google
-* --> FALTA: Mas logins como facebook, google+...
+* Nueva p&aacute;gina html con formulario para introducir direcci&oacute;n
+* Petici&oacute;n al servicio Google Maps a trav&eacute;s de su API
+* Visualizaci&oacute;n del mapa con marcador el la ciudad indicada.
+* Nueva p&aacute;gina para iniciar sesi&oacute;n con cuenta de Google
+* Inicio de sesi&oacute;n con Google mostrando informaci&oacute;n e imagen
+* Cierre de sesi&oacute;n de Google
+* --> FALTA: M&aacute;s login como facebook, google+...
 
 **1,5h aproximadamente realizar la tarea 5**
 
-## Tarea 6: Control de acceso via login
+## Tarea 6: Control de acceso v&iacute;a login
 
-* Añadir contador de accesos con una sesion activa (main)
-* Inicio de sesion con google o con credenciales en la aplicación web
-* Cierre de la sesion
-* Solo podra haber una sesion abierta (un usuario logeado),
-o con la cuenta registrada en la web, o con autenticacion con google
-* --> FALTA: las imagenes con los blobstore
+* A&ntilde;adir contador de accesos con una sesi&oacute;n activa (main)
+* Inicio de sesi&oacute;n con google o con credenciales en la aplicaci&oacute;n web
+* Cierre de la sesi&oacute;n
+* Solo podra haber una sesi&oacute;n abierta (un usuario logeado),
+o con la cuenta registrada en la web, o con autenticaci&oacute;n con google
+* Será necesario iniciar sesión para acceder a las fotos del album.
+* Nueva p&aacute;gina para poder subir fotos y almacenarlas
+* En el caso de ser usuario logeado,
+podr&aacute; acceder a las fotos p&uacute;blicas que han subido todos los usuarios,
+y adem&aacute;s ver las fotos privadas que ha subido con anterioridad.
 
 
-**3h aproximadamente realizar la tarea 6**
+**10h aproximadamente realizar la tarea 6**
+
+## Tarea 7: AÚN NOP...
 
 
 ## EXTRAS ##
--- dssw2016eneritz/album.py
+++ dssw2016eneritz/album.py
@@ -9,13 +9,14 @@ from google.appengine.ext import ndb
 from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.api import images
+import urllib
 
 class Image(ndb.Model):
     user = ndb.StringProperty()
     public = ndb.BooleanProperty()
     blob_key = ndb.BlobKeyProperty()
 
-class AlbumHandler(session_module.BaseSessionHandler, blobstore_handlers.BlobstoreUploadHandler):
+class PhotoUploadHandler(session_module.BaseSessionHandler, blobstore_handlers.BlobstoreUploadHandler):
     def get(self):
         logedUser = self.session.get('logedUser')
         googleUser = self.session.get('googleUser')
@@ -26,13 +27,13 @@ class AlbumHandler(session_module.BaseSessionHandler, blobstore_handlers.Blobsto
             user = googleUser
         values = {'logedUser': user}
         if user:
-            upload_url = blobstore.create_upload_url('/album/upload')
+            upload_url = blobstore.create_upload_url('/album/upload/')
             values = {'logedUser': user, 'upload_url': upload_url}
 
         lang = self.request.get('lang')#check language from get params
         if lang == '': lang = "es"
         self.response.out.write(
-                    template.render('static/elements/' + lang + '/album-' + lang + '.html', values))
+                    template.render('static/elements/' + lang + '/uploadImage-' + lang + '.html', values))
     def post(self):
         logedUser = self.session.get('logedUser')
         googleUser = self.session.get('googleUser')
@@ -48,16 +49,13 @@ class AlbumHandler(session_module.BaseSessionHandler, blobstore_handlers.Blobsto
     	    img = Image(user=user, public=self.request.get("access")=="public", blob_key=blob_info.key())
             img.put() #guardo el objeto Image
 
-        lang = self.request.get('lang')#check language from get params
-        if lang == '': lang = "es"
-        self.response.out.write(
-                    template.render('static/elements/' + lang + '/album-' + lang + '.html', values))
+        self.redirect('/album/')
 
 class ViewHandler(blobstore_handlers.BlobstoreDownloadHandler):
-	def get(self):
-		fotos= blobstore.BlobInfo.all()
-		for foto in fotos:
-            self.response.out.write('<img src="/album/serve/%s"></image></td>' % foto.key())
+    def get(self):
+        fotos = blobstore.BlobInfo.all()
+        for foto in fotos:
+            self.response.out.write('<img src="/album/serve/%s" width="75" height="75"></td>' % foto.key())
 
 class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
  	 def get(self, resource):
@@ -65,11 +63,62 @@ class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
 		blob_info = blobstore.BlobInfo.get(resource)
 		self.send_blob(blob_info)
 
+class AlbumHandler(session_module.BaseSessionHandler, blobstore_handlers.BlobstoreUploadHandler):
+    def get(self):
+        permission = self.request.get('permission')
+        logedUser = self.session.get('logedUser')
+        googleUser = self.session.get('googleUser')
+        user = ""
+        if logedUser:
+            user = logedUser
+        if googleUser:
+            user = googleUser
+        if user:
+            #get images
+            s = ""
+            i = 1
+            #images = blobstore.BlobInfo.all()
+            #FILTER IMAGES
+            if permission == "public":
+                images = Image.query().filter(Image.public == True)
+            else:
+                images = Image.query().filter(Image.public == False).filter(Image.user == logedUser)
+
+            for img in images:
+                s += '<img onmouseover="preview.src=img{0}.src" name="img{0}" src="/album/serve/{1}" alt="" />'.format(i,img.blob_key)
+                i += 1
+
+            values = {'logedUser': user}
+            lang = self.request.get('lang')#check language from get params
+            if lang == '': lang = "es"
+            self.response.out.write(
+                        template.render('static/elements/' + lang + '/album-' + lang + '.html', values))#html start
+            self.response.out.write(s)#images
+            self.response.out.write(#html end
+                '''
+                </div>
+                <div class="preview" align="center">
+                <img name="preview" src="/img/girl_computer_anime.png" alt=""/>
+                </div>
+
+                </div> <!-- Close the gallery div -->
+                <br><br><br>
+                </div></td></tr><tr><td colspan="6"><img src="/img/tail.jpg" class="backgroundImages" alt="tail"></td>
+                </tr></table></body></html>
+                '''
+            )
+        else:
+            error = "Tienes que iniciar sesion"
+            values = {'error': error}
+            lang = self.request.get('lang')#check language from get params
+            if lang == '': lang = "es"
+            self.response.out.write(
+                        template.render('static/elements/' + lang + '/album-' + lang + '.html', values))
 
 app = webapp2.WSGIApplication(
 [
     ('/album/', AlbumHandler),
-    ('/album/upload', AlbumHandler),
-    ('/album/download', ViewHandler),
-    ('/album/serve/([^/]+)?', ServeHandler)],
+    ('/album/upload/', PhotoUploadHandler),
+    ('/album/download/', ViewHandler),
+    ('/album/serve/([^/]+)?', ServeHandler),
 ], config = session_module.config, debug=True)
